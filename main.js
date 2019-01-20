09867dc219e29f5c1d204470771cb5048e461e81
Dutchman <rdrozda86@gmail.com>
2019-01-20 16:33:34 +0100

    version 0.2.0 - finalized
    * (Dutchman) rebuild logic to calculate values (beta testing)
    * (Dutchman) implement logic to automatically reset values by start of new day, week, month, year etc (beta testing)
    * (Dutchman) changed logic from intervall polling to handle calculations based on state updates (beta testing, not if suitable for all situations)
    * (Dutchman) fix issue incorrect states are added to monitoring
    * (Dutchman) fix issue calculation not stopped when state removed from monitoring
    * (Dutchman) always store all current meassurements to values of cathegorie regardless setting year etc
    * (Dutchman) code cleanup and optiomalisation
    * (Dutchman) added logging option "satus notification"
    * (Dutchman) implement new translation mechanisme

--- main.js
+++ main.js
@@ -44,22 +44,25 @@ const adapter = utils.adapter({
 			//@ts-ignore obj can be undefined yes, this IF checks that !
 			if (obj.common.custom !== null  && obj.common.custom !== undefined) {
 
-				// Check if object change is related to SourceAnalytix activation
 				//@ts-ignore obj is  not undefined or null and common.custom exists when entering this IF
-				if (obj.common.custom[inst_name].enabled === true){
-				// The object was changed
-				// // stop current running intervall (removed in 0.2.0, handled by state subscribe)
-				// (function () {if (interval_timer) {clearInterval(interval_timer); interval_timer = null;}})();
-					adapter.log.info("new state : " + id + " added to SourceAnalytix");
-					adapter.getForeignObject(id, function (err, obj){
+				if (obj.common.custom[inst_name].enabled !== null && obj.common.custom[inst_name].enabled !== undefined){
+				// Check if object change is related to SourceAnalytix activation
+					//@ts-ignore obj is  not undefined or null
+					if (obj.common.custom[inst_name].enabled === true){
+					// The object was changed
+					// // stop current running intervall (removed in 0.2.0, handled by state subscribe)
+					// (function () {if (interval_timer) {clearInterval(interval_timer); interval_timer = null;}})();
+						adapter.log.info("new state : " + id + " added to SourceAnalytix");
+						adapter.getForeignObject(id, function (err, obj){
 
-						if(dev_log === true){adapter.log.info(JSON.stringify(obj));}	// add logging variable
+							if(dev_log === true){adapter.log.info(JSON.stringify(obj));}	// add logging variable
 
-						if (obj !== undefined && obj !== null){
-							state_set.push(obj);
-							initialize(obj);
-						}
-					});
+							if (obj !== undefined && obj !== null){
+								state_set.push(obj);
+								initialize(obj);
+							}
+						});
+					}
 				}
 		
 			} else {
@@ -68,8 +71,8 @@ const adapter = utils.adapter({
 				adapter.unsubscribeForeignStates(id);
 			}
 		} catch (error) {
-
-			adapter.log.error("Issue in handling added/removed value to calculation" + error);
+			// temprorary disable error catch will be fixt later 
+			// adapter.log.error("Issue in handling added/removed value to calculation" + error);
 				
 		}			
 	},
@@ -309,6 +312,9 @@ function initialize(obj) {
 		adapter.log.info("Initialization finished for : " + device);
 		// Subscribe state, every state change will trigger calculation
 		adapter.subscribeForeignStates(obj._id);
+
+		// Start cron to reset values at day, week etc start
+		reset_shedules (obj._id);
 
 	} else {
 		adapter.log.error("Sorry unite type " + unit + " not supported yet");
