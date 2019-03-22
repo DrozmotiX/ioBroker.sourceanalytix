a2828d73d27873354e851b0fec49bcc43bde1fe0
Dutchman <rdrozda86@gmail.com>
2019-03-22 10:47:51 +0100

    try to fix unit testing

--- test/unit.js
+++ test/unit.js
@@ -2,4 +2,8 @@ const path = require("path");
 const { tests } = require("@iobroker/testing");
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+tests.unit(path.join(__dirname, ".."),{
+    defineMockBehavior(db, adapter) {
+        adapter.objects.getObjectView.returns({rows: []});
+    }
+});
