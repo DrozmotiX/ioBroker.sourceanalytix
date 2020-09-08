0dea9a0fe4050b32c04e307a34d84dac76904794
DutchmanNL <rdrozda86@gmail.com>
2020-09-08 18:28:09 +0200

    Fix issue with NULL values at day reset

--- main.js
+++ main.js
@@ -555,10 +555,10 @@ class Sourceanalytix extends utils.Adapter {
 					obj.common.custom = {};
 					obj.common.custom[this.namespace] = {
 						start_day: reading,
-						start_week: beforeReset.week === actualDate.week ? stateValues.week : reading,
-						start_month: beforeReset.month === actualDate.month ? stateValues.month : reading,
-						start_quarter: beforeReset.quarter === actualDate.quarter ? stateValues.quarter : reading,
-						start_year: beforeReset.year === actualDate.year ? stateValues.year : reading,
+						start_week: beforeReset.week === actualDate.week ? stateValues.start_week : reading,
+						start_month: beforeReset.month === actualDate.month ? stateValues.start_month : reading,
+						start_quarter: beforeReset.quarter === actualDate.quarter ? stateValues.start_quarter : reading,
+						start_year: beforeReset.year === actualDate.year ? stateValues.start_year : reading,
 					};
 
 					// Extend object with start value [type] & update memory
